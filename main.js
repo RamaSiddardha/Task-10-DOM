1e9dab5adc13e13e36449333e1d2ceba7da47210
RmaSiddardha <ramasiddardha@gmail.com>
2022-12-15 14:35:10 +0530

    fourth commit

--- main.js
+++ main.js
@@ -5,9 +5,17 @@ function getDetails(e){
     e.preventDefault()
 
     let UserName=document.getElementById('name').value
-    localStorage.setItem('User Name',UserName)
+    // localStorage.setItem('User Name',UserName)
 
     let Email=document.getElementById('email').value
-    localStorage.setItem('Email Id',Email)
+    // localStorage.setItem('Email Id',Email)
 
+    let details={
+        name:UserName,
+        Email_id:Email
+    }
+    Details_Stringify=JSON.stringify(details)
+    localStorage.setItem('Details',Details_Stringify)
+
+    console.log(Details_Stringify)
 }
