022bec230274d951e4c313d6cbf7e90320bc40c6
RmaSiddardha <ramasiddardha@gmail.com>
2022-12-15 16:00:40 +0530

    Task 12

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
 var myForm=document.querySelector('#my-form')
 myForm.addEventListener('submit',getDetails)
 
+var list=document.getElementById('users')
+
+
+
+
 function getDetails(e){
     e.preventDefault()
+    var UserName=document.getElementById('name').value
+    // localStorage.setItem(UserName,'User Name')
 
-    let UserName=document.getElementById('name').value
-    // localStorage.setItem('User Name',UserName)
+    var Email=document.getElementById('email').value
+    // localStorage.setItem(Email,'Email Id')
 
-    let Email=document.getElementById('email').value
-    // localStorage.setItem('Email Id',Email)
+    var li=document.createElement('li')
+    li.className='items'
+   
+    var Nametext= document.createTextNode(UserName)
+    var spacetext=document.createTextNode(" ")
+    var mailtext=document.createTextNode(Email)
 
-    let details={
-        name:UserName,
-        Email_id:Email
-    }
-    Details_Stringify=JSON.stringify(details)
-    localStorage.setItem('Details',Details_Stringify)
+    li.append(Nametext)
+    li.append(spacetext)
+    li.append(mailtext)
 
-    console.log(Details_Stringify)
+    list.append(li)
+    
+// li.appendChild(document.createTextNode())
+    
+
+
+    // let details={
+    //     name:UserName,
+    //     Email_id:Email
+    // }
+    // Details_Stringify=JSON.stringify(details)
+    localStorage.setItem(Email,UserName)
+
+    // console.log(Details_Stringify)
 }
+
